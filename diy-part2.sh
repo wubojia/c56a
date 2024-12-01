8bdded7db16fc57502c17dd4243795b8564ab944
RedFree <ltby126yx@126.com>
2024-12-01 09:15:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,9 @@
 # 管理地址
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
+# 主机名
+#sed -i 's/OpenWrt/OpenRoute/g' package/base-files/files/bin/config_generate
+
 # 默认主题
 rm -rf feeds/kenzo/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
