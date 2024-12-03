42708723211f6a5a3183cfc53ec9af1edf0f5411
RedFree <ltby126yx@126.com>
2024-12-03 11:41:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # 主机名
-#sed -i 's/OpenWrt/OpenRoute/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenRoute/g' package/base-files/files/bin/config_generate
 
 # 默认主题
 rm -rf feeds/kenzo/luci-theme-argon
