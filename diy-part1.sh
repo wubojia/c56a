cf663b32dfbd458241ac8c5fcba4076d0ce93c13
wubojia <wubojia@163.com>
2025-02-06 19:15:22 +0800

    add feed openclash src

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# add openclash
